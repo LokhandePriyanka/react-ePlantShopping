fd9161e4c7b194cf3f1a4d78501464e27620ad81
LokhandePriyanka <89181467+LokhandePriyanka@users.noreply.github.com>
2024-10-22 16:26:59 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/react-ePlantShopping/', // Ensure this matches your repository name
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  }
 });
